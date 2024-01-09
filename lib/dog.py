6865489984d0723ee39ff0703acb51e1428e3d78
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-09 18:52:07 -0500

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -10,6 +9,37 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
-
+# # # Dog and lib/dog.py
+# # # Define a name property for your Dog class. The name must be of type str and between 1 and 25 characters. 
+# # Your __init__ method should receive a default argument for name.
+# # # If the name is invalid, the setter method should print()
+#  "Name must be string between 1 and 25 characters."
+# # # Define a breed property for your Dog class. Your __init__ method
+#  should receive a default argument for breed.
+# # # If the breed is invalid, the setter method should print() "Breed must be in list of approved breeds."
+# #  The breed must be in the following list of dog breeds:
+# # # approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French
 class Dog:
-    pass
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+    name= property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    breed= property(get_breed, set_breed)
+    
--- lib/person.py
+++ lib/person.py
@@ -14,6 +14,34 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
-
+# Person and lib/person.py
+# Define a name property for your Person class. The name
+#    must be of type str and between 1 and 25 characters. The name should be converted to title caseLinks to an external site.
+# before it is saved. Your __init__ method should receive a default argument for name.
+# If the name is invalid, the setter method should print() "Name must be string between 1 and 25 characters."
 class Person:
-    pass
+    def __init__(self, name='tyler', job='Marketing'):
+        self.name = name
+        self.job = job
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        if isinstance(name, str) and 1<= len(name) <= 25:
+            title_name = name.title()
+            self._name = title_name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+    name= property(get_name, set_name)
+        
+    
+#     Define a job property for your Person class. Your __init__ method should receive a default argument for job.
+# If the job is invalid, the setter method should print() "Job must be in list of approved jobs." 
+# The job must be in the following list of jobs:
+    def get_job(self):
+        return self._job
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+    job= property(get_job, set_job)
